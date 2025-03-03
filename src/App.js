9bfb35703dfe8a71d3e4b285dbff1a6e3e94e949
khaledsho3er <97366301+khaledsho3er@users.noreply.github.com>
2025-03-03 15:50:05 +0200

    Add AuthProvider context and enhance user session management with Firebase

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/login";
 import Register from "./Pages/register";
 import Home from "./Pages/home";
-
+import AuthProvider from "./utils/authContext";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />  
-        <Route path="/signup" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
